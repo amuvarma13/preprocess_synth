8b1dfd2fc42821a4ed51ee361af0d6bf13fa8f68
Amartya Varma <amartya.varma@gmail.com>
2024-04-30 23:17:01 +0100

    f

--- generate_data_file.py
+++ generate_data_file.py
@@ -17,7 +17,7 @@ def find_wavs_with_non_empty_txt(directory):
                 wav_path = os.path.join(root, file)
                 # Change the extension from .wav to .txt
                 txt_path = os.path.splitext(wav_path)[0] + '.txt'
-                
+
                 # Check if the txt file exists and is not empty
                 if os.path.exists(txt_path) and os.path.getsize(txt_path) > 0:
                     # Add the filename without extension and without path
@@ -25,22 +25,28 @@ def find_wavs_with_non_empty_txt(directory):
 
     return matched_files
 
-#get all file root names
+
+# get all file root names
 all_files = find_wavs_with_non_empty_txt(directory)
 
-#get texts
+print(f'Found {len(all_files)} files with non-empty txt files')
+
+# get texts
+
+
 def read_txt_files(file_roots, directory_base="outputs"):
     texts = []
-    
+
     # Iterate over each file root provided
     for file_root in file_roots:
         # Extract voice_id and construct the directory path
-        voice_id = file_root.split('_')[0]  # Assuming the voice_id is the prefix before the first underscore
+        # Assuming the voice_id is the prefix before the first underscore
+        voice_id = file_root.split('_')[0]
         directory = os.path.join(directory_base, voice_id)
-        
+
         # Construct the full path to the txt file
         txt_file_path = os.path.join(directory, f"{file_root}.txt")
-        
+
         # Check if the txt file exists
         if os.path.exists(txt_file_path):
             # Read the contents of the txt file
@@ -49,30 +55,31 @@ def read_txt_files(file_roots, directory_base="outputs"):
                 texts.append(content)
         else:
             print(f"File not found: {txt_file_path}")
-            
+
     return texts
+
 
 texts = read_txt_files(all_files)
 
+print(f'Generated texts for {len(texts)} files')
 
 
 def phonemize_texts(texts, language='en-us'):
     phonemized_texts = []
 
     # Phonemize each text in the list
-    for text in texts:
-        phonemized_text = phonemize(
-            text,
-            backend="espeak",
-            language=language,
-            strip=False,
-            preserve_punctuation=True,
-            with_stress=True
-        )
-        phonemized_texts.append(phonemized_text)
-    
+
+    phonemized_texts = phonemize(
+        texts,
+        backend="espeak",
+        language=language,
+        strip=False,
+        preserve_punctuation=True,
+        with_stress=True
+    )
+
     return phonemized_texts
 
 
 p_txts = phonemize_texts(texts)
-print(p_txts)
+print(f'Phonemized {len(p_txts)} texts')
